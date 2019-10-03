ef4322d4287d4634bdc962b93e79b2618f2c0710
Thomas Rodgers <thomasrodgers271@gmail.com>
2019-10-03 12:30:15 -0700

    changed route

---
+++ assets/js/contact.js
@@ -0,0 +1,51 @@
+$(document).ready(() => {
+    const enc = new TextEncoder();
+    const form = $('form');
+    form.submit(event => {
+        event.preventDefault();
+        message = {};
+        window.crypto.subtle.importKey(
+            'jwk', //can be 'jwk' (public or private), 'spki' (public only), or 'pkcs8' (private only)
+            {   //this is an example jwk key, other key types are Uint8Array objects
+                kty: 'RSA',
+                e: 'AQAB',
+                n: 'rZP3F3IxZvCHVeOyDWe86Y32RdgQ61LCFBu4IMtfN5Qke6IlIAKsKXbv25T5bk7AKzRFFpuWKSJfqtqPC-o1-2y-jfTcrYHRo_tCTDf8q2BeGOt2eUYKfgCkcoTVjokn9G_TLRXBhc7KiYMtx2Pnbz2g8zRDCrglepSwHRPsQ3rLO8RW5cuwT0YjxHLThaqOY1I_6psauttJAzGoD50kMhqcrJAPRXNPa4wAAuEXPIEb66iFskOrwvQ2Q81RFTppcXhusTeC-za5-NQppJZXh_PPkW6suV3sPxxMWYUWRBF4FVZi4UPh4VDIhKR-2NnldSPw3ScW7TPPhAJB2-u6Fw',
+                alg: 'RSA-OAEP-256',
+                ext: true,
+            },
+            {   //these are the algorithm options
+                name: 'RSA-OAEP',
+                hash: { name: 'SHA-256' }, //can be 'SHA-1', 'SHA-256', 'SHA-384', or 'SHA-512'
+            },
+            false, //whether the key is extractable (i.e. can be used in exportKey)
+            ['encrypt'] //'encrypt' or 'wrapKey' for public key import or
+            //'decrypt' or 'unwrapKey' for private key imports
+        ).then(publicKey => {
+            const promises = [], names = [];
+            form.children('input, textarea').each((i, element) => {
+                const elem = $(element);
+                //returns a publicKey (or privateKey if you are importing a private key)
+                names.push(elem.attr('name'));
+                promises.push(window.crypto.subtle.encrypt(
+                    { name: 'RSA-OAEP' },
+                    publicKey,
+                    enc.encode(elem.val())
+                ));
+                Promise.all(promises).then(encrypted => {
+                    for (let i = 0; i < encrypted.length; ++i) {
+                        //returns an ArrayBuffer containing the encrypted data
+                        console.log(new Uint8Array(encrypted[i]));
+                        message[names[i]] = encrypted[i];
+                    }
+                    $.post('https://contactee.herokuapp.com/', { data: message }, () => {
+                        console.log(message);
+                    });
+                }).catch(function (err) {
+                    console.error(err);
+                });
+            });
+        }).catch(function (err) {
+            console.error(err);
+        });
+    });
+});
